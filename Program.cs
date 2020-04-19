bead3f7282e019c0e8dcc7369103781b410e37d1
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 22:56:11 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradosARadianes
+{
+    class Program
+    {
+        //Funcion para concertir grados a radianes
+        static double ConvertirGrados(double grad)
+        {
+            double rad = 0.0f;
+
+            rad = grad * 3.14 / 180;
+
+            return rad;
+        }
+        //Funcion principal
+        static void Main(string[] args)
+        {
+            double grados = 0.0f, radianes = 0.0f;
+
+            Console.Write("Escriba el numero en grados que desea convertir: ");
+
+            grados = Convert.ToDouble(Console.ReadLine());
+
+            radianes = ConvertirGrados(grados);
+
+            Console.WriteLine("{0} grados = {1} radianes", grados, radianes);
+
+            Console.ReadKey();
+            
+        }
+    }
+}
